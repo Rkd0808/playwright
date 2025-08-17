59731522b45ad2192182835725afd0d7ce8fc81e
Rohit Das <kumarrohit0808@gmail.com>
2025-08-17 19:24:22 +0530

    fix yml

--- config/env.js
+++ config/env.js
@@ -1,7 +1,8 @@
 // config/env.js
-import path from 'path';
 import { fileURLToPath } from 'url';
 import dotenv from 'dotenv';
+
+const path = require('path');
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
